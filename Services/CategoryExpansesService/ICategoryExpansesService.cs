865b38eacebaf029166192702c641292dbe3ceac
Мышкин Даниил <dsmyshkin@asuproject.ru>
2024-02-09 21:04:08 +0500

    add base infrastructure for expanses category

---
+++ Services/CategoryExpansesService/ICategoryExpansesService.cs
@@ -0,0 +1,12 @@
+﻿using plusminus.Dtos.CategoryExpenses;
+using plusminus.Models;
+
+namespace plusminus.Services.CategoryExpansesService
+{
+    public interface ICategoryExpansesService
+    {
+        Task<ServiceResponse<List<GetCategoryExpansesDto>>> AddCategoryExpanses(AddCategoryExpansesDto newCategoryExpanses);
+        Task<ServiceResponse<GetCategoryExpansesDto>> UpdateCategoryExpanses(UpdateCategoryExpansesDto updatedCategoryExpanses);
+        Task<ServiceResponse<List<GetCategoryExpansesDto>>> DeleteCategoryExpansesById(int id);
+    }
+}
