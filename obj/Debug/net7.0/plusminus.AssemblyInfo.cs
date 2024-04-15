86d4b9ab94cf51ea4e052079c7c879aa2d8ce3d5
psukitp <jfellertv@gmail.com>
2024-04-15 19:50:57 +0500

    idk wrg

--- obj/Debug/net7.0/plusminus.AssemblyInfo.cs
+++ obj/Debug/net7.0/plusminus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("plusminus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2566a0b2215832843d06eee507ec397b278c991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df0f05495c8f8c2bc523333c437bd95c8fc2ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("plusminus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("plusminus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
